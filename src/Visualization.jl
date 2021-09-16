1b3c3fd11d6579ced6e5fa3721c37c0ddd0a073e
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2021-09-16 09:47:36 +0200

    Updated the visualization routine to overlay the continent's outline

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -1,6 +1,7 @@
 
 
 function showGif(field,title_in="Solar Forcing of the Earth: t = ",minval=-46.073107944313556,maxval=32.52216883281448)
+    World = read_geography("The_World_Outline.dat")*(-80)
     @gif for t in 1:size(field,3)
         title = title_in * lpad(string(floor(t*365/48)), 3, ' ') * " days"
     
@@ -11,7 +12,8 @@ function showGif(field,title_in="Solar Forcing of the Earth: t = ",minval=-46.07
             yflip=true,
             title=title,
             xlabel="longitude [°]",ylabel="latitude [°]")
-    
+        contour!(LinRange(-180,180,128),LinRange(-90,90,65),transpose(World),xlim=(-180,180), ylim=(-90, 90),levels=-40:40,seriesalpha=0.05)
+        
         fpath = "png/field/" * lpad(string(t), 3, '0') * ".png"
         println(fpath)
         savefig(fpath)
