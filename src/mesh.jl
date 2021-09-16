546c3d3bb481049a9b565450747d489d2f9320c4
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2021-09-16 17:46:59 +0200

    Renamed all variables to julia style

--- src/mesh.jl
+++ src/mesh.jl
@@ -1,13 +1,12 @@
 struct Mesh
-    nx::Int         # Number of DOFs in x (Longitude)
-    ny::Int         # Number of DOFs in y (Latitude)
-    dof::Int
-    h::Float64
-    sh2::Float64
-    geom::Float64
-    csc2::Array{Float64,1}
-    cot::Array{Float64,1}
-    area::Array{Float64,1}
+    nx::Int                     # Number of DOFs in x (Longitude)
+    ny::Int                     # Number of DOFs in y (Latitude)
+    dof::Int                    # Total number of DOFs: nx * ny
+    h::Float64                  # Grid size [radians]: π/(ny-1)
+    geom::Float64               # Geometrical parameter at poles
+    csc2::Array{Float64,1}      # Metric term for the transformation to spherical coordinates: cosc²(θ), where θ is the colatitude angle
+    cot::Array{Float64,1}       # Metric term for the transformation to spherical coordinates: cot(θ), where θ is the colatitude angle
+    area::Array{Float64,1}      # Area of each cell on the surface of the sphere (*only* depends on latitude)
 end
 
 function Mesh(nx=128)
@@ -15,7 +14,6 @@ function Mesh(nx=128)
     dof = nx*ny
 
     h = pi / (ny-1) # Uniform grid size in radians
-    sh2 = 1 / h^2
 
     # Trigonometric definitions for inner degrees of freedom
     csc2 = zeros(Float64,ny) # csc^2(theta), where theta is the colatitude angle (from the pole)
@@ -39,5 +37,5 @@ function Mesh(nx=128)
 
     geom = sin(0.5*h)/area[1]
 
-    return Mesh(nx,ny,dof,h,sh2,geom,csc2,cot,area)
+    return Mesh(nx,ny,dof,h,geom,csc2,cot,area)
 end
