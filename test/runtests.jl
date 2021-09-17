f71ea4f10a6967ab7935aba4c48cc5a4dd5327ab
Michael Schlottke-Lakemper <michael@sloede.com>
2021-09-17 17:10:29 +0200

    Improve test coverage (#5)

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,7 @@ EXAMPLES_DIR = joinpath(pathof(Klimakoffer) |> dirname |> dirname, "examples")
 
     @test isapprox(GlobTemp, 14.484963368768746, atol=1e-12)
   end
+
   test_file = "equilibrium_temperature_co2.jl" 
   @testset "$test_file" begin
     println("")
@@ -23,6 +24,7 @@ EXAMPLES_DIR = joinpath(pathof(Klimakoffer) |> dirname |> dirname, "examples")
 
     @test isapprox(GlobTemp, 14.484963368768978, atol=1e-12)
   end
+
   test_file = "equilibrium_temperature_co2_transient.jl"
   @testset "$test_file" begin
     println("")
@@ -31,5 +33,19 @@ EXAMPLES_DIR = joinpath(pathof(Klimakoffer) |> dirname |> dirname, "examples")
     @test_nowarn include(joinpath(EXAMPLES_DIR, test_file))
 
     @test isapprox(sol.mean_temperature_yearly[end], 14.563278065969344, atol=1e-12)
+  end
+
+  @testset "Printing types to the REPL" begin
+    mesh = Mesh()
+    @test_nowarn show(stdout, mesh)
+    println(stdout)
+
+    model = Model(mesh, 48)
+    @test_nowarn show(stdout, model)
+    println(stdout)
+
+    discretization = Discretization(mesh, model, 48)
+    @test_nowarn show(stdout, discretization)
+    println(stdout)
   end
 end
