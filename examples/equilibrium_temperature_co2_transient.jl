9132648935e686b3c8a4b05a46f12882f0b74247
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2021-09-17 14:04:32 +0200

    Added time-step-wise temperature output for compute_evolution! and modified tests accordingly

--- examples/equilibrium_temperature_co2_transient.jl
+++ examples/equilibrium_temperature_co2_transient.jl
@@ -10,7 +10,6 @@ co2_concentration_start = 315.0
 co2_concentration_yearly_delta = 1.39
 
 co2_concentration_yearly = zeros(Float64,year_delta)
-mean_temperature_yearly = zeros(Float64, year_delta)
 
 mesh = Mesh()
 model = Model(mesh, NT; co2_concentration = co2_concentration_start)
@@ -21,7 +20,7 @@ for years = year_start:year_end
   co2_concentration_yearly[iter] = co2_concentration_start + co2_concentration_yearly_delta * (iter - 1)
 end
 println("First compute initial temperature distribution when equilibrium is reached")
-mean_temperature_yearly[1] = compute_equilibrium!(discretization; verbose = false)   
+initial_temp = compute_equilibrium!(discretization; verbose = false)   
 println("Start evolution of temperature...")
-compute_evolution!(discretization, co2_concentration_yearly, mean_temperature_yearly, year_start; verbose=true) 
+sol = compute_evolution!(discretization, co2_concentration_yearly, year_start; verbose=true) 
 println("Is it getting hotter?")
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,19 +5,31 @@ using Printf
 EXAMPLES_DIR = joinpath(pathof(Klimakoffer) |> dirname |> dirname, "examples")
 
 @time @testset "Klimakoffer" begin
-  @testset "equilibrium_temperature_1950.jl" begin
-    @test_nowarn include(joinpath(EXAMPLES_DIR, "equilibrium_temperature_1950.jl"))
+  test_file = "equilibrium_temperature_1950.jl"
+  @testset "$test_file" begin
+    println("")
+    println("Running ",test_file)
+    println("")
+    @test_nowarn include(joinpath(EXAMPLES_DIR, test_file))
 
     @test isapprox(GlobTemp, 14.484963368768746, atol=1e-12)
   end
-  @testset "equilibrium_temperature_co2.jl" begin
-    @test_nowarn include(joinpath(EXAMPLES_DIR, "equilibrium_temperature_co2.jl"))
+  test_file = "equilibrium_temperature_co2.jl" 
+  @testset "$test_file" begin
+    println("")
+    println("Running ",test_file)
+    println("")
+    @test_nowarn include(joinpath(EXAMPLES_DIR, test_file))
 
     @test isapprox(GlobTemp, 14.484963368768978, atol=1e-12)
   end
-  @testset "equilibrium_temperature_co2_transient.jl" begin
-    @test_nowarn include(joinpath(EXAMPLES_DIR, "equilibrium_temperature_co2_transient.jl"))
+  test_file = "equilibrium_temperature_co2_transient.jl"
+  @testset "$test_file" begin
+    println("")
+    println("Running ",test_file)
+    println("")
+    @test_nowarn include(joinpath(EXAMPLES_DIR, test_file))
 
-    @test isapprox(mean_temperature_yearly[end], 14.563278065969344, atol=1e-12)
+    @test isapprox(sol.mean_temperature_yearly[end], 14.563278065969344, atol=1e-12)
   end
 end
