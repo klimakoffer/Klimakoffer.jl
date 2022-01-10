4ba26043339e6be71e882655e8ff5edb1b0a4e6d
Niklas Oeztekin <noeztek1@smail.uni-koeln.de>
2022-01-10 12:14:05 +0100

    fixed int issue for land cover test

--- examples/land_coverage_from_scaled_map.jl
+++ examples/land_coverage_from_scaled_map.jl
@@ -2,7 +2,7 @@ using Klimakoffer
 
 world = Klimakoffer.read_geography("../input/world/The_World128x65.dat", 128, 65)
 
-rand_long = rand(128:2048)
+rand_long = rand(128:2:2048)
 
 upscaled_world = Klimakoffer.nn_interpolation(world, rand_long)
 
