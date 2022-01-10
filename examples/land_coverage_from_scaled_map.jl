fa8aa27630041875365feac29bb00410a5f94117
Niklas Oeztekin <noeztek1@smail.uni-koeln.de>
2022-01-10 11:41:30 +0100

    fixed test for land cover

--- examples/land_coverage_from_scaled_map.jl
+++ examples/land_coverage_from_scaled_map.jl
@@ -1,10 +1,10 @@
 using Klimakoffer
 
-world = Klimakoffer.read_geography("./input/world/The_World128x65.dat", 128, 65)
+world = Klimakoffer.read_geography("../input/world/The_World128x65.dat", 128, 65)
 
 rand_long = rand(128:2048)
 
-upscaled_world = nn_interpolation(world, rand_long)
+upscaled_world = Klimakoffer.nn_interpolation(world, rand_long)
 
 (nlong,nlat) = size(upscaled_world)
 
