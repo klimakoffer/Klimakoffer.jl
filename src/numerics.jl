b3d4e379ff6d804f3a3e209a71421a8301c4b8cd
Hendrik Ranocha <ranocha@users.noreply.github.com>
2022-01-12 09:14:27 +0100

    Apply suggestions from code review
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/numerics.jl
+++ src/numerics.jl
@@ -36,6 +36,7 @@ function compute_equilibrium!(discretization; max_years=100, rel_error=2e-5, ver
             old_time_step = (time_step == 1) ? num_steps_year : time_step - 1
             update_rhs!(rhs, mesh, num_steps_year, time_step, view(annual_temperature, :, old_time_step), model, last_rhs)
                         
+            # Use in-place operation `ldiv!` instead of `\` to avoid allocations
             ldiv!(view(annual_temperature, :, time_step), lu_decomposition, rhs)
 
             annual_temperature[1:nx, time_step] .= annual_temperature[1, time_step]
@@ -103,6 +104,7 @@ function compute_evolution!(discretization, co2_concentration_at_step, year_star
             old_time_step = (time_step == 1) ? num_steps_year : time_step - 1
             update_rhs!(rhs, mesh, num_steps_year, time_step, view(annual_temperature, :, old_time_step), model, last_rhs)
                         
+            # Use in-place operation `ldiv!` instead of `\` to avoid allocations
             ldiv!(view(annual_temperature, :, time_step), lu_decomposition, rhs)
 
             annual_temperature[1:nx, time_step] .= annual_temperature[1, time_step]
