7d73f3b5d025643e5e6aa5a264be7c978b5d430a
Niklas Oeztekin <noeztek1@smail.uni-koeln.de>
2022-01-24 09:08:00 +0100

    change paths in test image_to_world.jl

--- examples/image_to_world.jl
+++ examples/image_to_world.jl
@@ -2,9 +2,9 @@ using Klimakoffer
 
 Klimakoffer.images_to_maps(joinpath(@__DIR__,"examples", "test_instances"), joinpath(@__DIR__,"examples", "test_instances"), true, 1, 10, 11, 10, 10, 190, 210)
 
-world = Klimakoffer.read_geography(joinpath(@__DIR__, "..",  "examples", "test_instances", "The_World_from_image10x6_3.dat" ), 10, 6)
+world = Klimakoffer.read_geography(joinpath(@__DIR__,  "examples", "test_instances", "The_World_from_image10x6_3.dat" ), 10, 6)
 
-ref = Klimakoffer.read_geography(joinpath(@__DIR__, "..","examples","test_instances", "test_image_result.dat"), 10, 6)
+ref = Klimakoffer.read_geography(joinpath(@__DIR__,"examples","test_instances", "test_image_result.dat"), 10, 6)
 
 diff = ref-world
 
